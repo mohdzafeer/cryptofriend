3516c05ac3beec371d3749b5f575e2ae902b6f99
Zafeer <mohammadzafeer2610@gmail.com>
2024-09-06 15:00:34 +0530

    react icon added to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,12 @@
 
+import { FaReact } from "react-icons/fa";
+
+
 const Footer = () => {
 
     return (
         <div className='flex justify-center items-center h-28 bg-teal-400 text-gray-900 font-bold mt-10 font-serif'>
-            <p>Made with ❤️ by <span onClick={()=>window.open('https://portfolio-nine-rust-42.vercel.app/')} className='font-semibold hover:underline cursor-pointer duration-200 '>Mohammad Zafeer</span></p>
+            <p className="flex items-center gap-2">Made with <FaReact/> by <span onClick={()=>window.open('https://portfolio-nine-rust-42.vercel.app/')} className='font-semibold hover:underline cursor-pointer duration-200 '>Mohammad Zafeer</span></p>
         </div>
     )
 }
